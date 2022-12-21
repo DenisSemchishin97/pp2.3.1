ab961445b8b975314d50a01ff58e855de215ff8d
DenisSemchishin97 <semchishin.denis@gmail.com>
2022-12-21 21:52:51 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,54 @@
+package web.dao;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+@Repository
+public class UserDaoImpl implements UserDao {
+@PersistenceContext
+private EntityManager entityManager;
+    @Override
+
+    public List<User> getAllUsers() {
+        TypedQuery<User> query = entityManager.createQuery("from User", User.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+
+    }
+
+    @Override
+    public User getUser(int id) {
+       return entityManager.find(User.class,id);
+    }
+
+    @Override
+    public void update(int id, User user) {
+    User user1=getUser(id);
+    user1.setName(user.getName());
+    user1.setSurname(user.getSurname());
+    user1.setDepartment(user.getDepartment());
+    user1.setSalary(user.getSalary());
+    entityManager.merge(user1);
+    }
+
+    @Override
+    public void delete(int id) {
+        String s="delete from Users where id=:userId";
+        TypedQuery<User>query= (TypedQuery<User>) entityManager.createQuery("delete from User where id=:userId");
+        query.setParameter("userId",id);
+        query.executeUpdate();
+    }
+}
