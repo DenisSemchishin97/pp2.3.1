3f991e559d769942f0fc74cac7265dfa1d918d3c
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2022-12-21 21:54:16 +0300

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import web.dao.UserDao;
 import web.entity.User;
 import web.service.UserService;
 
